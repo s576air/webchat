f3275f995b09c36fe1f8a43959e2f4d58c21bcd7
s576air <s576ai@naver.com>
2024-09-14 22:06:29 +0900

    springboot 생성
    스프링부트를 사용해 프로젝트를 생성했습니다. 설계 단계에 없던 jdbc가 추가되었지만, 설계는 변경하지 않았습니다.

---
+++ src/test/java/com/s576air/webchat/WebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s576air.webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
